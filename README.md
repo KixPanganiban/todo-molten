303b017a210e5f48348d9ff7226fbc92ee78c18c
Kix Panganiban <kixpanganiban@protonmail.com>
2018-10-02 09:43:58 +0800

    :memo: Format with Black and add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# todo-molten
+![https://moltenframework.com/v0.5.2/_static/logo.svg](https://moltenframework.com/v0.5.2/_static/logo.svg)
+
+A simple TODO backend written in [Molten](https://moltenframework.com/), a fast web framework taking advantage of Python 3 type annotations to expose a powerful Dependency Injection system. DB powered by SQLite. This was written for Todobackend.com, see passing specs here: [https://www.todobackend.com/specs/index.html?https://todo-molten.herokuapp.com/v1/todos/](https://www.todobackend.com/specs/index.html?https://todo-molten.herokuapp.com/v1/todos/)
+
+Sample instance running at: https://todo-molten.herokuapp.com/v1/todos/
--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""A Molten web server for Todobackend.com
+"""
+__author__ = "Kix Panganiban <github.com/kixpanganiban>"
+
 import json
 import sqlite3
 from contextlib import contextmanager
@@ -5,8 +10,23 @@ from inspect import Parameter
 from typing import Any, Callable, Iterator, List, Optional, Tuple, Union
 
 from molten import (
-    HTTP_201, HTTP_204, HTTP_403, HTTP_404, App, Component, Field, Header, HTTPError, Include,
-    Middleware, Request, Response, ResponseRenderer, ResponseRendererMiddleware, Route, schema
+    HTTP_201,
+    HTTP_204,
+    HTTP_403,
+    HTTP_404,
+    App,
+    Component,
+    Field,
+    Header,
+    HTTPError,
+    Include,
+    Middleware,
+    Request,
+    Response,
+    ResponseRenderer,
+    ResponseRendererMiddleware,
+    Route,
+    schema,
 )
 from molten.app import BaseApp
 from molten.http.headers import HeadersDict
@@ -14,12 +34,17 @@ from molten.renderers import JSONRenderer
 
 
 class DB:
+    """Database adapter class, in this case uses a SQLite backend.
+    """
+
     def __init__(self) -> None:
         self._db = sqlite3.connect("molten.db")
         self._db.row_factory = sqlite3.Row
 
         with self.get_cursor() as cursor:
-            cursor.execute("create table if not exists todos(title text, completed bool, \"order\" int)")
+            cursor.execute(
+                'create table if not exists todos(title text, completed bool, "order" int)'
+            )
 
     @contextmanager
     def get_cursor(self) -> Iterator[sqlite3.Cursor]:
@@ -36,6 +61,9 @@ class DB:
 
 
 class DBComponent:
+    """Database component for Dependency Injection.
+    """
+
     is_cacheable = True
     is_singleton = True
 
@@ -48,6 +76,10 @@ class DBComponent:
 
 @schema
 class Todo:
+    """Todo object definition for the API. Schema will automatically populate
+    fields implicitly.
+    """
+
     id: Optional[int] = Field(response_only=True)
     title: Optional[str]
     order: Optional[int]
@@ -56,33 +88,49 @@ class Todo:
 
 
 class TodoManager:
+    """TodoManager ORM abstraction for the DB.
+    """
+
     def __init__(self, db: DB) -> None:
         self.db = db
 
     def _map_todo(self, data: sqlite3.Row):
+        """Maps an sqlite3.Row object into a dict, converting the bool values
+        and populating the url field.
+        """
         data = dict(data)
-        data['completed'] = False if data['completed'] == 0 else True
-        data['url'] = f"https://todo-molten.herokuapp.com/v1/todos/{data['id']}"
+        data["completed"] = False if data["completed"] == 0 else True
+        data["url"] = f"https://todo-molten.herokuapp.com/v1/todos/{data['id']}"
         return data
 
     def create(self, todo: Todo) -> Todo:
+        """Insert a todo into the DB.
+        """
         with self.db.get_cursor() as cursor:
-            cursor.execute("insert into todos(title, completed, \"order\") values(?, ?, ?)", [
-                todo.title,
-                todo.completed,
-                todo.order
-            ])
+            cursor.execute(
+                'insert into todos(title, completed, "order") values(?, ?, ?)',
+                [todo.title, todo.completed, todo.order],
+            )
 
             return self.get_by_id(cursor.lastrowid)
 
     def get_all(self) -> List[Todo]:
+        """Retrieve all todos from the DB.
+        """
         with self.db.get_cursor() as cursor:
-            cursor.execute("select rowid as id, title, completed, \"order\" from todos order by \"order\" desc")
+            cursor.execute(
+                'select rowid as id, title, completed, "order" from todos order by "order" desc'
+            )
             return [Todo(**self._map_todo(data)) for data in cursor.fetchall()]
 
     def get_by_id(self, todo_id: int) -> Optional[Todo]:
+        """Retrieve a single todo from the DB by id.
+        """
         with self.db.get_cursor() as cursor:
-            cursor.execute("select rowid as id, title, completed, \"order\" from todos where rowid = ? limit 1", [todo_id])
+            cursor.execute(
+                'select rowid as id, title, completed, "order" from todos where rowid = ? limit 1',
+                [todo_id],
+            )
             data = cursor.fetchone()
             if data is None:
                 return None
@@ -90,30 +138,40 @@ class TodoManager:
             return Todo(**self._map_todo(data))
 
     def update_by_id(self, todo_id: int, updates: Todo) -> Optional[Todo]:
+        """Update a single todo in the DB by id. Uses existing todo to
+        perform a partial update for fields that are not provided.
+        """
         todo = self.get_by_id(todo_id)
         if todo:
-            for attr_key in ('title', 'completed', 'order'):
+            for attr_key in ("title", "completed", "order"):
                 attr_val = getattr(updates, attr_key, None)
                 if attr_val is not None:
                     setattr(todo, attr_key, attr_val)
             with self.db.get_cursor() as cursor:
                 cursor.execute(
-                    "update todos set title = ?, completed = ?, \"order\" = ? where rowid = ?",
-                    [todo.title, todo.completed, todo.order, todo_id]
-                    )
+                    'update todos set title = ?, completed = ?, "order" = ? where rowid = ?',
+                    [todo.title, todo.completed, todo.order, todo_id],
+                )
                 return self.get_by_id(todo_id)
         return None
 
     def delete_by_id(self, todo_id: int) -> None:
+        """Delete a single todo by id.
+        """
         with self.db.get_cursor() as cursor:
             cursor.execute("delete from todos where rowid = ?", [todo_id])
 
     def delete_all(self) -> None:
+        """Delete all todos in the table.
+        """
         with self.db.get_cursor() as cursor:
             cursor.execute("delete from todos")
 
 
 class TodoManagerComponent:
+    """TodoManager component for Dependency Injection.
+    """
+
     is_cacheable = True
     is_singleton = True
 
@@ -125,18 +183,26 @@ class TodoManagerComponent:
 
 
 def options_todos() -> str:
+    """Handles OPTIONS /v1/todos
+    """
     return HTTP_204, None
 
 
 def list_todos(manager: TodoManager) -> List[Todo]:
+    """Handles GET /v1/todos
+    """
     return manager.get_all()
 
 
 def options_todo(todo_id: str) -> str:
+    """Handles OPTIONS /v1/todos/{id}
+    """
     return HTTP_204, None
 
 
 def get_todo(todo_id: str, manager: TodoManager) -> Todo:
+    """Handles GET /v1/todos/{id}
+    """
     todo = manager.get_by_id(int(todo_id))
     if todo is None:
         raise HTTPError(HTTP_404, {"error": f"todo {todo_id} not found"})
@@ -144,10 +210,14 @@ def get_todo(todo_id: str, manager: TodoManager) -> Todo:
 
 
 def create_todo(todo: Todo, manager: TodoManager) -> Tuple[str, Todo]:
+    """Handles POST /v1/todos
+    """
     return HTTP_201, manager.create(todo)
 
 
 def update_todo(todo_id: str, todo: Todo, manager: TodoManager) -> Todo:
+    """Handles PATCH /v1/todos/{id}
+    """
     todo = manager.update_by_id(todo_id, todo)
     if todo is None:
         raise HTTPError(HTTP_404, {"error": f"todo {todo_id} not found"})
@@ -155,12 +225,18 @@ def update_todo(todo_id: str, todo: Todo, manager: TodoManager) -> Todo:
 
 
 def delete_todo(todo_id: str, manager: TodoManager) -> Tuple[str, None]:
+    """Handles DELETE /v1/todos/{id}
+    """
     manager.delete_by_id(int(todo_id))
     return HTTP_204, None
 
+
 def delete_all(manager: TodoManager) -> Tuple[str, None]:
+    """Handles DELETE /v1/todos/
+    """
     manager.delete_all()
     return []
+
 
 class CORSMiddleware:
     """Middleware to inject CORS headers.
@@ -169,17 +245,20 @@ class CORSMiddleware:
     def __call__(self, handler: Callable[..., Any]) -> Callable[..., Response]:
         def handle(app: BaseApp, request: Request) -> Response:
             headers: HeadersDict = {
-                'access-control-allow-origin': '*',
-                'access-control-allow-headers': 'Accept, Content-Type',
-                'access-control-allow-methods': '*'
+                "access-control-allow-origin": "*",
+                "access-control-allow-headers": "Accept, Content-Type",
+                "access-control-allow-methods": "*",
             }
             response = handler()
             response.headers.add_all(headers)
             return response
+
         return handle
 
+
 class PlainTextRenderer(JSONRenderer):
-    """A plaintext response renderer.
+    """A plaintext response renderer, subclassed from JSONRenderer, to comply
+    with Todobackend's plaintext accept header.
     """
 
     mime_type = "text/plaint"
@@ -189,44 +268,33 @@ class PlainTextRenderer(JSONRenderer):
 
     def render(self, status: str, response_data: Any) -> Response:
         content = json.dumps(response_data, default=self.default)
-        return Response(status, content=content, headers={
-            "content-type": "text/plain",
-        })
+        return Response(status, content=content, headers={"content-type": "text/plain"})
 
 
-components: List[Component] = [
-    DBComponent(),
-    TodoManagerComponent(),
-]
+components: List[Component] = [DBComponent(), TodoManagerComponent()]
 
 
-middleware: List[Middleware] = [
-    CORSMiddleware(),
-    ResponseRendererMiddleware()
-]
+middleware: List[Middleware] = [CORSMiddleware(), ResponseRendererMiddleware()]
 
-renderers: List[ResponseRenderer] = [
-    JSONRenderer(),
-    PlainTextRenderer()
-]
+renderers: List[ResponseRenderer] = [JSONRenderer(), PlainTextRenderer()]
 
 
 routes: List[Union[Route, Include]] = [
-    Include("/v1/todos", [
-        Route("/", options_todos, method="OPTIONS"),
-        Route("/", list_todos),
-        Route("/", create_todo, method="POST"),
-        Route("/", delete_all, method="DELETE"),
-        Route("/{todo_id}", options_todo, method="OPTIONS"),
-        Route("/{todo_id}", get_todo),
-        Route("/{todo_id}", delete_todo, method="DELETE"),
-        Route("/{todo_id}", update_todo, method="PATCH")
-    ]),
+    Include(
+        "/v1/todos",
+        [
+            Route("/", options_todos, method="OPTIONS"),
+            Route("/", list_todos),
+            Route("/", create_todo, method="POST"),
+            Route("/", delete_all, method="DELETE"),
+            Route("/{todo_id}", options_todo, method="OPTIONS"),
+            Route("/{todo_id}", get_todo),
+            Route("/{todo_id}", delete_todo, method="DELETE"),
+            Route("/{todo_id}", update_todo, method="PATCH"),
+        ],
+    )
 ]
 
 app = App(
-    components=components,
-    middleware=middleware,
-    renderers=renderers,
-    routes=routes,
+    components=components, middleware=middleware, renderers=renderers, routes=routes
 )
